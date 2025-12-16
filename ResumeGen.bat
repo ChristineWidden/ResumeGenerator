d0df47acc1eeab8af26ffd872247cb84582e9368
TheShinySnivy <magicwand452@gmail.com>
2025-12-15 16:25:36 -0800

    Some changes to resume look including skills table, some notes atop some files explaining their purpose, some style alterations, some changes to the .bat launch file

--- ResumeGen.bat
+++ ResumeGen.bat
@@ -1,18 +1,29 @@
 @echo off
-cd /d "D:\Projects\Programming\Other\Resume Generation"
+echo opening resume generation folder
+cd /d "D:\Programming\Other\Resume Generation"
 
 REM Activate virtual environment
+echo Activating virtual environment
 call .\venv\Scripts\activate
 
 REM Start the Node server
+echo Starting the Node server
 start "" node app.js
 
 REM Wait a bit for the server to spin up
-timeout /t 2 /nobreak >nul
+echo Waiting for the server to start
+timeout /t 3 /nobreak >nul
 
 REM Open the browser to the local server
+echo Opening the browser to the server
 start "" http://localhost:3000
 
 REM Open the folder where generated files are saved
-start "" explorer "D:\Projects\Programming\Other\Resume Generation"
+echo Opening the working folder
+start "" explorer "D:\Programming\Other\Resume Generation"
+
+echo Opening the resume documents folder
 start "" explorer "D:\Documents\Official\Resume"
+
+echo Done!
+timeout /t 10 /nobreak >nul
